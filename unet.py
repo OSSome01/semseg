ed16d9caa14e9701f6ec0394a73d12c785550e28
Omkar Sargar <sargar.o@northeastern.edu>
2023-04-15 21:17:15 -0400

    add vanila unet architecture

--- unet.py
+++ unet.py
@@ -1,37 +1,78 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+    
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Down, self).__init__()
+        self.mpconv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+    
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Up, self).__init__()
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, 2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+    
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, 1)
+    
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
 class UNet(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_channels, num_classes):
         super(UNet, self).__init__()
-        # Downsample layers
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-        # Upsample layers
-        self.upconv1 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.upconv3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        # Output layer
-        self.conv_out = nn.Conv2d(64, out_channels=num_classes, kernel_size=1)
-
+        self.inc = DoubleConv(num_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)
+        self.up1 = Up(1024, 512)
+        self.up2 = Up(512, 256)
+        self.up3 = Up(256, 128)
+        self.up4 = Up(128, 64)
+        self.outc = OutConv(64, num_classes)
+    
     def forward(self, x):
-        # Downsample path
-        x1 = F.relu(self.conv1(x))
-        x2 = F.max_pool2d(x1, kernel_size=2, stride=2)
-        x2 = F.relu(self.conv2(x2))
-        x3 = F.max_pool2d(x2, kernel_size=2, stride=2)
-        x3 = F.relu(self.conv3(x3))
-        x4 = F.max_pool2d(x3, kernel_size=2, stride=2)
-        x4 = F.relu(self.conv4(x4))
-        # Upsample path
-        x = F.relu(self.upconv1(x4))
-        x = torch.cat([x, x3], dim=1)
-        x = F.relu(self.upconv2(x))
-        x = torch.cat([x, x2], dim=1)
-        x = F.relu(self.upconv3(x))
-        x = torch.cat([x, x1], dim=1)
-        # Output
-        x = self.conv_out(x)
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.outc(x)
         return x
