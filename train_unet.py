786ca58ee030f9c9015ac69d6ff5a63cebadc074
Omkar Sargar <sargar.o@northeastern.edu>
2023-04-18 08:36:03 -0400

    update train_unet.py

--- train_unet.py
+++ train_unet.py
@@ -7,70 +7,78 @@ import torchvision.transforms as transforms
 from data_prep import RUGD
 from unet import UNet
 
-# Define the device to be used for training
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if __name__ == '__main__':
 
-# Define the transforms to be applied to the data
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),    # Resize the image to 256x256
-    transforms.ToTensor(),            # Convert the image to a PyTorch tensor
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))    # Normalize the pixel values to [-1, 1]
-])
+    # Define the device to be used for training
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Load the RUGD dataset
-trainset = RUGD(root_dir='/home/ossome/semantic_segmentation/sem_seg/RUGD', split='train', transform=transform)
-testset = RUGD(root_dir='/home/ossome/semantic_segmentation/sem_seg/RUGD', split='test', transform=transform)
+    # Define the transforms to be applied to the data
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)),    # Resize the image to 256x256
+        transforms.ToTensor(),            # Convert the image to a PyTorch tensor
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))    # Normalize the pixel values to [-1, 1]
+    ])
 
-# Create data loaders for the datasets
-trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+    # Load the RUGD dataset
+    trainset = RUGD(root_dir='E:\DL\Final Project\RUGD', split='train', transform=transform)
+    testset = RUGD(root_dir='E:\DL\Final Project\RUGD', split='test', transform=transform)
 
-# Define the model
-model = UNet(num_channels=3, num_classes=25)
-model.to(device)
+    # Create data loaders for the datasets
+    trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
-# Define the loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # Define the model
+    model = UNet(num_channels=3, num_classes=25)
+    model.to(device)
 
-# Train the model
-num_epochs = 10
-for epoch in range(num_epochs):
-    # Train the model on the training set
-    model.train()
-    train_loss = 0.0
-    for images, labels in trainloader:
-        print('images', images.shape)
-        print('labels', labels.shape)
-        images = images.to(device)
-        labels = nn.functional.one_hot(labels.long(), 25)
-        labels = labels.to(device)
-        
-        optimizer.zero_grad()
-        
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        
-        train_loss += loss.item() * images.size(0)
-        
-    train_loss /= len(trainloader.dataset)
-    
-    # Test the model on the test set
-    model.eval()
-    test_loss = 0.0
-    for images, labels in testloader:
-        images = images.to(device)
-        labels = labels.to(device)
-        
-        with torch.no_grad():
+    # Define the loss function and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    # Train the model
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        # Train the model on the training set
+        model.train()
+        train_loss = 0.0
+        for images, labels in trainloader:
+            # print('images', images.shape)
+            labels = labels.permute(0,3,1,2)
+            # print('labels', labels.shape)
+            images = images.to(device)
+            labels = nn.functional.one_hot(labels.long(), 25)
+            labels = labels.to(device)
+            # print('labels', labels.shape)
+
+            optimizer.zero_grad()
+            
             outputs = model(images)
-            loss = criterion(outputs, labels)
+            outputs = nn.functional.softmax(outputs, dim=1)  # apply softmax to output
+            _, predicted = torch.max(outputs, dim=1)  # get predicted class along channel dimension
+            loss = criterion(outputs, torch.squeeze(predicted, dim=1))  # use predicted class as label
+            loss.backward()
+            optimizer.step()
+            
+            train_loss += loss.item() * images.size(0)
+            
+        train_loss /= len(trainloader.dataset)
         
-        test_loss += loss.item() * images.size(0)
+        # Test the model on the test set
+        model.eval()
+        test_loss = 0.0
+        for images, labels in testloader:
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            with torch.no_grad():
+                outputs = model(images)
+                outputs = nn.functional.softmax(outputs, dim=1)  # apply softmax to output
+                _, predicted = torch.max(outputs, dim=1)  # get predicted class along channel dimension
+                loss = criterion(outputs, torch.squeeze(predicted, dim=1))  # use predicted class as label
+            
+            test_loss += loss.item() * images.size(0)
+            
+        test_loss /= len(testloader.dataset)
         
-    test_loss /= len(testloader.dataset)
-    
-    # Print the loss for the current epoch
-    print(f'Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
+        # Print the loss for the current epoch
+        print(f'Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
